4962343b6bb3f5306688e62901f43e1266e00d63
mozammal01 <mozammalhaq01@gmail.com>
2026-01-27 11:37:41 +0600

    Add nodemailer for email verification and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import { notFound } from "./middlewares/notFound";
+import errorHandler from "./middlewares/globalErrorHandler";
 
-const app = express();
+const app: Application = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.APP_URL || "http://localhost:4000", // client side url
+  credentials: true
+}))
+
 app.use(express.json());
 
 app.all('/api/auth/*splat', toNodeHandler(auth));
@@ -13,5 +19,8 @@ app.all('/api/auth/*splat', toNodeHandler(auth));
 app.get("/", (req, res) => {
   res.send("Hello from Education Bridge Server");
 });
+app.use(notFound)
+app.use(errorHandler)
+
 
 export default app;
