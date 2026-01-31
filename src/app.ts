b8588137c45a0298817be161546ccc737a59771c
mozammal01 <mozammalhaq01@gmail.com>
2026-01-31 15:21:46 +0600

    Update CORS configuration to allow local development access

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,8 @@ import { reviewsRouter } from "./modules/routes/reviews.routes";
 const app: Application = express();
 
 app.use(cors({
-  origin: process.env.APP_URL || "https://education-bridge-client.vercel.app",
+  // origin: process.env.APP_URL || "https://education-bridge-client.vercel.app",
+  origin:"http://localhost:3000",
   credentials: true
 }))
 
